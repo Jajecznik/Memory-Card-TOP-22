58690bdd442102cdae84e18110d3f219eea2417c
Jajecznik <mcjajecznik@gmail.com>
2024-11-09 04:21:59 +0100

    fetching data

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+  return (
+    <section>
+      <div>
+        IMAGE
+      </div>
+      <div>
+        <p>NAME</p>
+      </div>
+    </section>
+  );
+}
+
+export default Card;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="flex justify-between p-4">
+      <div>
+        <p>MEMORY CHECKER</p>
+        <p>To get a point click on card but just once!</p>
+      </div>
+      <div>
+        <p>Your score:</p>
+        <p>Your best:</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
